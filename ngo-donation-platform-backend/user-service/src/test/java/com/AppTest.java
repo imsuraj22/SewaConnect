5d895114e16218e0bead9e16958fb3af4875eb52
Suraj <surajgaikwad9881313856@gmail.com>
2025-08-07 17:53:52 +0530

    Microservice setup is done

---
+++ ngo-donation-platform-backend/user-service/src/test/java/com/AppTest.java
@@ -0,0 +1,16 @@
+package com;
+
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+/**
+ * Unit test for simple App.
+ */
+// ✅ New JUnit 5
+public class AppTest {
+    @Test
+    public void testApp() {
+        Assertions.assertTrue(true);
+    }
+}
